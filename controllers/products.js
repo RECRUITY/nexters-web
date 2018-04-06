f13165a01e7adf040288ecfd9e74a4314c0a2938
MinhoCheon <mino@MinhoCheonui-MacBook-Pro.local>
2018-04-06 21:34:54 +0900

    Add creating product api

--- controllers/products.js
+++ controllers/products.js
@@ -5,6 +5,12 @@ const Product = require('../models/Product');
 
 const router = express.Router();
 
+/**
+ * @api {get} /api/products Get
+ * @apiGroup Product
+ * @apiName get products
+ * @apiDescription 존재하는 모든 products를 반환한다.
+ */
 router.get('/', (req, res, next) => {
   Product
     .find()
@@ -14,6 +20,24 @@ router.get('/', (req, res, next) => {
       }
       return res.status(HttpStatus.OK).json({ products });
     });
+});
+
+/**
+ * @api {post} /api/products Post
+ * @apiGroup Product
+ * @apiName Create New Product
+ * @apiDescription 새로운 Product를 추가한다.
+ */
+router.post('/', (req, res, next) => {
+  (new Product({
+    title: req.body.title,
+    description: req.body.description,
+  })).save((err, product) => {
+    if (err) {
+      return next(err);
+    }
+    return res.status(HttpStatus.OK).json({ product });
+  });
 });
 
 module.exports = router;
