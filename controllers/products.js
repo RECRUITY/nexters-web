23cdaaf5d6cba13b7f26b1a4320db8f75351fb2e
MinhoCheon <mino@MinhoCheonui-MacBook-Pro.local>
2018-04-06 21:49:18 +0900

    Add updating product api

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const Product = require('../models/Product');
 const router = express.Router();
 
 /**
- * @api {get} /api/products Get
+ * @api {get} /api/products GET
  * @apiGroup Product
  * @apiName get products
  * @apiDescription 존재하는 모든 products를 반환한다.
@@ -23,7 +23,7 @@ router.get('/', (req, res, next) => {
 });
 
 /**
- * @api {post} /api/products Post
+ * @api {post} /api/products POST
  * @apiGroup Product
  * @apiName Create New Product
  * @apiDescription 새로운 Product를 추가한다.
@@ -38,6 +38,27 @@ router.post('/', (req, res, next) => {
     }
     return res.status(HttpStatus.OK).json({ product });
   });
+});
+
+/**
+ * @api {put} /api/products/:id PUT
+ * @apiGroup Product
+ * @apiName Update Product
+ * @apiDescription 해당 id의 프로덕트를 갱신한다.
+ */
+router.put('/:id', (req, res, next) => {
+  const { title, description } = req.body;
+  Product.findByIdAndUpdate(
+    req.params.id,
+    { title, description },
+    { new: true },
+    (err, product) => {
+      if (err) {
+        return next(err);
+      }
+      return res.status(HttpStatus.OK).json({ product });
+    },
+  );
 });
 
 module.exports = router;
