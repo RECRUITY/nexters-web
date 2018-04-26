346243734bb95ae29e403bd1ed09ef389c8288cc
Minho Cheon <mino@Minhoui-MacBook-Pro.local>
2018-04-27 01:28:40 +0900

    Added group model for view

---
+++ views/admin/src/app/models/group.model.ts
@@ -0,0 +1,25 @@
+import { File, IFileResponse } from './file.model';
+
+export class Product {
+  readonly id: string = '';
+  readonly name: string = '';
+  readonly file: File = new File();
+
+  constructor(
+    fields?: {
+      _id?: string,
+      name?: string,
+      file?: IFileResponse,
+    },
+  ) {
+    if (fields) {
+      this.id = fields._id;
+      this.name = fields.name;
+      this.file = new File(fields.file);
+    }
+  }
+
+  hasImage() {
+    return this.file.id && this.file.image;
+  }
+}
