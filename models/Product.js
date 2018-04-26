3aa54e9afb246180216f0996746b0d5a89d7bd78
Minho Cheon <mino@Minhoui-MacBook-Pro.local>
2018-04-27 00:11:47 +0900

    Set name of Gropu model to unique

--- models/Product.js
+++ models/Product.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const ProductScheme = new mongoose.Schema({
   title: {
     type: String,
+    unique: true,
     required: true,
   },
   description: String,
