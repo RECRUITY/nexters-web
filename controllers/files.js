e7c28a0a0e657bbfaaff2a06d0cefb98eff35a45
Minho Cheon <mino@Minhoui-MacBook-Pro.local>
2018-05-02 22:51:51 +0900

    Added caching function for load image

--- controllers/files.js
+++ controllers/files.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const apicache = require('apicache');
 
 const router = express.Router();
+
+const cache = apicache.middleware;
 
 /**
  * @api {get} /api/files/:id GET
@@ -8,7 +11,7 @@ const router = express.Router();
  * @apiName get file
  * @apiDescription 해당 id의 File을 반환한다.
  */
-router.get('/:id', (req, res) => {
+router.get('/:id', cache('720 hours'), (req, res) => {
   const readstream = global.gfs.createReadStream({ _id: req.params.id });
   readstream.pipe(res);
 });
